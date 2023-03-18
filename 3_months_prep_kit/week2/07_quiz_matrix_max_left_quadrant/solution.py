c675c88345ef0edfc0ff01dfda6ea1780694e8c8
unfamiliarish <89762294+unfamiliarish@users.noreply.github.com>
2023-03-17 18:50:53 -0700

    simplest solution doesn't work

--- 3_months_prep_kit/week2/07_quiz_matrix_max_left_quadrant/solution.py
+++ 3_months_prep_kit/week2/07_quiz_matrix_max_left_quadrant/solution.py
@@ -2,28 +2,14 @@ import numpy as np
 
 
 def flippingMatrix(matrix) -> int:
-    # Write your code here
-    # og_matrix = np.array(matrix)
-    np_matrix = np.array(matrix)
+    flattened = [num for row in matrix for num in row]
+    sorted_nums = sorted(flattened)
 
-    n = len(np_matrix) // 2
-    # swap lower rows
-    for i in range(n):
-        if sum(np_matrix[i + n][0:n]) > sum(np_matrix[i + n][n:]):
-            np_matrix[i + n] = np_matrix[i + n][::-1]
-    # swap right cols
-    for j in range(n):
-        if sum(np_matrix[n:, j + n]) > sum(np_matrix[:n, j + n]):
-            np_matrix[:, j + n] = np_matrix[:, j + n][::-1]
-    # swap upper rows
-    for i in range(n):
-        if sum(np_matrix[i][n:]) > sum(np_matrix[i][0:n]):
-            np_matrix[i] = np_matrix[i][::-1]
+    n = len(matrix) // 2
+    max = sum(sorted_nums[n:])
 
-    # get upper left sum
-    upper_left_sum = sum(sum(np_matrix[:n, :n]))
-    # breakpoint()
-    return upper_left_sum
+    breakpoint()
+    return max
 
 
 matrix_0 = [[1, 2], [3, 4]]
